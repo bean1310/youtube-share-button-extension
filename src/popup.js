5f061b17b5d6422537c743410f6555f8375f803d
bean1310 <bean@beanlog.xyz>
2022-05-15 16:58:25 +0900

    とりあえず、コア機能だけ実装

--- src/popup.js
+++ src/popup.js
@@ -2,111 +2,56 @@
 
 import './popup.css';
 
-(function() {
-  // We will make use of Storage API to get and store `count` value
-  // More information on Storage API can we found at
-  // https://developer.chrome.com/extensions/storage
-
-  // To get storage access, we have to mention it in `permissions` property of manifest.json file
-  // More information on Permissions can we found at
-  // https://developer.chrome.com/extensions/declare_permissions
-  const counterStorage = {
-    get: cb => {
-      chrome.storage.sync.get(['count'], result => {
-        cb(result.count);
-      });
-    },
-    set: (value, cb) => {
-      chrome.storage.sync.set(
+(function () {
+  
+  /**
+   * Copy to clipboard a content data of current an active tab.
+   */
+  function copy_content_data() {
+    console.log("CLICK");
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      console.log(tabs);
+      chrome.tabs.sendMessage(tabs[0].id,
         {
-          count: value,
+          type: 'QUERY',
+          payload: {
+            message: 'title'
+          }
         },
-        () => {
-          cb();
-        }
-      );
-    },
-  };
-
-  function setupCounter(initialValue = 0) {
-    document.getElementById('counter').innerHTML = initialValue;
-
-    document.getElementById('incrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'INCREMENT',
-      });
-    });
-
-    document.getElementById('decrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'DECREMENT',
-      });
-    });
-  }
-
-  function updateCounter({ type }) {
-    counterStorage.get(count => {
-      let newCount;
-
-      if (type === 'INCREMENT') {
-        newCount = count + 1;
-      } else if (type === 'DECREMENT') {
-        newCount = count - 1;
-      } else {
-        newCount = count;
-      }
-
-      counterStorage.set(newCount, () => {
-        document.getElementById('counter').innerHTML = newCount;
-
-        // Communicate with content script of
-        // active tab by sending a message
-        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-          const tab = tabs[0];
-
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              type: 'COUNT',
-              payload: {
-                count: newCount,
-              },
-            },
-            response => {
-              console.log('Current count value passed to contentScript file');
-            }
-          );
+        /**
+         * 
+         * @param {*} response content data
+         */
+        function (response) {
+          document.getElementById('contentTitle').innerText = response.title;
+          document.getElementById('contentUrl').innerText = response.url;
+          navigator.clipboard.writeText(response.title + "\n" + response.url + "\n\n" + new Date().toLocaleString("ja-JP"));
         });
-      });
     });
   }
 
-  function restoreCounter() {
-    // Restore count value
-    counterStorage.get(count => {
-      if (typeof count === 'undefined') {
-        // Set counter value as 0
-        counterStorage.set(0, () => {
-          setupCounter(0);
+  function get_content_data() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      console.log(tabs);
+      chrome.tabs.sendMessage(tabs[0].id,
+        {
+          type: 'QUERY',
+          payload: {
+            message: 'title'
+          }
+        },
+        /**
+         * 
+         * @param {*} response content data
+         */
+        function (response) {
+          document.getElementById('contentTitle').innerText = response.title;
+          document.getElementById('thumbImg').setAttribute("src", response.image_url);
         });
-      } else {
-        setupCounter(count);
-      }
     });
   }
 
-  document.addEventListener('DOMContentLoaded', restoreCounter);
+  document.getElementById("copyToClipboard").addEventListener('click', copy_content_data);
+  get_content_data();
 
-  // Communicate with background file by sending a message
-  chrome.runtime.sendMessage(
-    {
-      type: 'GREETINGS',
-      payload: {
-        message: 'Hello, my name is Pop. I am from Popup.',
-      },
-    },
-    response => {
-      console.log(response.message);
-    }
-  );
 })();
